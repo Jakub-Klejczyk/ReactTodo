9c3fdd2850dd2db8958b6f2970d760193f8ca6b2
Bartosz <b.pazdur@yahoo.com>
2022-06-27 17:26:59 +0200

    Bug fixing
    Fix invalid react injects and fix temporar sass loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Input from "./components/Input/Input";
-import Button from "./components/Button/Button";
 import ListItem from "./components/ListItem/ListIteam";
 import Form from "./components/Form/Form";
 
+
 const initialStateItems = [<ListItem>task2</ListItem>];
+
 
 class App extends React.Component {
   state = {
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "./Button.module.scss";
+// import styles from "./Button.module.scss";
 
 const Button = ({ children, addTask }) => (
-  <button className={styles.button} onClick={addTask}>
+  <button onClick={addTask}>
     {children}
   </button>
 );
